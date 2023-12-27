ec860366ddf53dd406127762bf4e145090b9656d
nkarki13731 <nkarki13731@upei.ca>
2023-12-27 16:19:54 -0400

    Initial commit

---
+++ src/Ticktactoee.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import  java.awt.event.*;
+class Fdemo extends Frame implements ActionListener{
+    Fdemo{
+        setLayout(null);
+        setVisible(true);
+        setSize(800,600);
+        setLocation(400.100);
+        setBackground(Color.black);
+        setForeground(Color.white);
+    }
+
+
+
+    @java.lang.Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+
+    }
+}
+
+public class Ticktactoee {
+    public static void main(String[] args) {
+
+    }
+}
