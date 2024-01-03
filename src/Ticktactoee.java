a018a50c671376fdb0d67e1709b34c44164f9f14
nkarki13731 <nkarki13731@upei.ca>
2024-01-03 12:57:46 -0400

    changed the data structure from array to 2D array

--- src/Ticktactoee.java
+++ src/Ticktactoee.java
@@ -1,52 +1,76 @@
 import java.awt.*;
 import  java.awt.event.*;
-class Fdemo extends Frame implements ActionListener{
-    Button b[] = new Button[9];
-    Button b1 ;
+class Fdemo extends Frame implements ActionListener {
+    Button b[][] = new Button[3][3];
+    //    Button b[] = new Button[9];
+    Button b1;
     int k = 0;
     int x = 8;
     int y = 28;
     int a = 0; //0 or X
-    int z = 0, z1 = 0, z2 =0, z3=0, z4=0, z5=0, z6=0, z7=0, z8=0;
+    int z = 0, z1 = 0, z2 = 0, z3 = 0, z4 = 0, z5 = 0, z6 = 0, z7 = 0, z8 = 0;
+    private Frame secondFrame;
 
-    Fdemo(){
+    Fdemo() {
         setLayout(null);
         setVisible(true);
-        setSize(800,600);
-        setLocation(400,100);
-        setBackground(Color.white);
-        setForeground(Color.black);
-        for(int i = 0; i<3; ++i){
-            for(int j = 0; j<3; ++j){
-                b[k] = new Button();
-                b[k].setSize(100,100);
-                b[k].setLocation(x,y);
-                b[k].setFont(new Font("",Font.BOLD,40));
-                add(b[k]);
-                b[k].addActionListener(this);
-                b[k].setBackground(new Color(255,141,28));//orange color
-                k++;
+        setSize(800, 600);
+        setLocation(400, 100);
+        setBackground(Color.BLACK);
+        setForeground(Color.WHITE);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                b[i][j] = new Button();
+                b[i][j].setSize(100, 100);
+                b[i][j].setLocation(x, y);
+                b[i][j].setFont(new Font("", Font.BOLD, 40));
+                add(b[i][j]);
+                b[i][j].addActionListener(this);
+                b[i][j].setBackground(new Color(255, 141, 28));//orange color
                 x+=100;
 
-
             }
-            x =8;
-            y =y+100;
+            x = 8;
+            y = y + 100;
+
         }
+//
+//        for(int i = 0; i<3; ++i){
+//            for(int j = 0; j<3; ++j){
+//                b[k] = new Button();
+//                b[k].setSize(100,100);
+//                b[k].setLocation(x,y);
+//                b[k].setFont(new Font("",Font.BOLD,40));
+//                add(b[k]);
+//                b[k].addActionListener(this);
+//                b[k].setBackground(new Color(255,141,28));//orange color
+//                k++;
+//
+//
+//
+//            }
+
+
+
         b1 = new Button("New Game");
-        b1.setSize(150,40);
-        b1.setLocation(500,300);
-        b1.setFont(new Font("",Font.BOLD,20));
+        b1.setSize(150, 40);
+        b1.setLocation(500, 300);
+        b1.setFont(new Font("", Font.BOLD, 20));
 
         add(b1);
         b1.addActionListener(this);
     }
+
     @java.lang.Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
-        // settifng button for newGame
+        //settifng button for newGame
         if(e.getSource() == b1){
-            for(int i = 0; i < 9; i++ ){
-                b[i].setLabel("");
+            for(int i = 0; i < 3; i++ ){
+                for(int j = 0 ; j<3; j++ ){
+                    b[i][j].setLabel("");
+                }
+
             }
             z = 0; z1 = 0; z2 =0; z3=0; z4=0; z5=0; z6=0; z7=0; z8=0;
         }
@@ -163,9 +187,10 @@ class Fdemo extends Frame implements ActionListener{
 
 
     }
+
 }
 
-public class Ticktactoee {  
+public class Ticktactoee {
     public static void main(String[] args) {
         Fdemo f = new Fdemo();
 
