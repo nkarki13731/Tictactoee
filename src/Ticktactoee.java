c6dfaafeb4f0790ebfbc95861a2218b2fd787d54
nkarki13731 <nkarki13731@upei.ca>
2024-05-07 15:38:44 -0600

    done

--- src/Ticktactoee.java
+++ src/Ticktactoee.java
@@ -1,200 +1,341 @@
-import java.awt.*;
-import  java.awt.event.*;
-class Fdemo extends Frame implements ActionListener {
-    private Button b[][] = new Button[3][3];
-    private Button b1;
-    private int start = 0;
-    private int x = 8;
-    private int y = 28;
-    private int a = 0; //0 or X
-    private int z = 0, z1 = 0, z2 = 0, z3 = 0, z4 = 0, z5 = 0, z6 = 0, z7 = 0, z8 = 0;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Fdemo extends Frame implements ActionListener {
+    Button[] b = new Button[9];
+    Button b1;
+    int k = 0;
+    int x = 8;
+    int y = 28;
+    int a = 0;
+    int z = 0;
+    int z1 = 0;
+    int z2 = 0;
+    int z3 = 0;
+    int z4 = 0;
+    int z5 = 0;
+    int z6 = 0;
+    int z7 = 0;
+    int z8 = 0;
+    int l = 70;
 
     Fdemo() {
-        this.setLayout(null);
-        this. setVisible(true);
+        this.setLayout((LayoutManager)null);
+        this.setVisible(true);
         this.setSize(800, 600);
         this.setLocation(400, 100);
-        this.setBackground(Color.BLACK);
-        this.setForeground(Color.WHITE);
+        this.setBackground(Color.black);
+        this.setForeground(Color.white);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                this.b[i][j] = new Button();
-                this.b[i][j].setSize(100, 100);
-                this.b[i][j].setLocation(x, y);
-                this.b[i][j].setFont(new Font("", Font.BOLD, 40));
-                this.add(b[i][j]);
-                this.b[i][j].addActionListener(this);
-                this.b[i][j].setBackground(new Color(255, 141, 28));//orange color
-                this.x+=100;
-
+        for(int var1 = 1; var1 <= 3; ++var1) {
+            for(int var2 = 1; var2 <= 3; ++var2) {
+                this.b[this.k] = new Button();
+                this.b[this.k].setSize(100, 100);
+                this.b[this.k].setLocation(this.x, this.y);
+                this.b[this.k].setFont(new Font("", 1, 40));
+                this.add(this.b[this.k]);
+                this.b[this.k].addActionListener(this);
+                this.b[this.k].setBackground(new Color(255, 141, 28));
+                ++this.k;
+                this.x += 100;
             }
-            this.x = 8;
-            this.y = y + 100;
 
+            this.x = 8;
+            this.y += 100;
         }
 
         this.b1 = new Button("New Game");
         this.b1.setSize(150, 40);
         this.b1.setLocation(500, 300);
-        this.b1.setFont(new Font("", Font.BOLD, 20));
-
-        this.add(b1);
+        this.b1.setFont(new Font("", 1, 20));
+        this.b1.setForeground(Color.black);
+        this.add(this.b1);
         this.b1.addActionListener(this);
     }
 
-    @java.lang.Override
-    public void actionPerformed(java.awt.event.ActionEvent e) {
-        //settifng button for newGame
-        if(e.getSource() == this.b1){
-            this.start = 1;
-            for(int i = 0; i < 3; i++ ){
-                for(int j = 0 ; j<3; j++ ){
-                    this.b[i][j].setLabel("");
-                }
-
+    public void actionPerformed(ActionEvent var1) {
+        if (var1.getSource() == this.b1) {
+            for(int var2 = 0; var2 <= 8; ++var2) {
+                this.b[var2].setLabel("");
             }
-            this.z = 0; this.z1 = 0; this.z2 =0; this.z3=0; this.z4=0; this.z5=0; this.z6=0; this.z7=0; this.z8=0;
-            this.a = 0;
+
+            this.z = 0;
+            this.z1 = 0;
+            this.z2 = 0;
+            this.z3 = 0;
+            this.z4 = 0;
+            this.z5 = 0;
+            this.z6 = 0;
+            this.z7 = 0;
+            this.z8 = 0;
         }
 
-        // setting other buttons as per the player input
-        else if (this.start == 1) {
-
-          if (e.getSource() == this.b[0][0] && z!=1) {
-            if(this.a%2==0){
-                this.b[0][0].setLabel("O");
-                this.a++;
-                this.z++;
-            }
-            else {
-                b[0][0].setLabel("X");
-                a++;
-                z++;
-            }
-
-        }
-
-        else if  (e.getSource() == this.b[0][1] && z1!=1){
-            if(this.a%2==0){
-                this.b[0][1].setLabel("O");
-                this.a++;
-                this.z1++;
-            }
-            else {
-                this.b[0][1].setLabel("X");
-                this.a++;
-                this.z1++;
+        if (var1.getSource() == this.b[0] && this.z == 0) {
+            if (this.a % 2 == 0) {
+                this.b[0].setLabel("O");
+                ++this.z;
+                ++this.a;
+            } else {
+                this.b[0].setLabel("X");
+                ++this.z;
+                ++this.a;
             }
         }
 
-
-        else if (e.getSource() == this.b[0][2] && z2!=1){
-            if(this.a%2==0){
-                this.b[0][2].setLabel("O");
-                this.a++;
-                this.z2++;
-            }
-            else {
-                this.b[0][2].setLabel("X");
-                this.a++;
-                this.z2++;
+        if (var1.getSource() == this.b[1] && this.z1 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[1].setLabel("O");
+                ++this.z1;
+                ++this.a;
+            } else {
+                this.b[1].setLabel("X");
+                ++this.z1;
+                ++this.a;
             }
         }
-        else if(e.getSource() == this.b[1][0] && z3!=1){
-            if(this.a%2==0){
-                this.b[1][0].setLabel("O");
-                this.a++;
-                this.z3++;
-            }
-            else {
-                this.b[1][0].setLabel("X");
-                this.a++;
-                this.z3++;
+
+        if (var1.getSource() == this.b[2] && this.z2 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[2].setLabel("O");
+                ++this.z2;
+                ++this.a;
+            } else {
+                this.b[2].setLabel("X");
+                ++this.z2;
+                ++this.a;
             }
         }
-        else if(e.getSource() == this.b[1][1] && z4!=1){
-            if(this.a%2==0){
-                this.b[1][1].setLabel("O");
-                this.a++;
-                this.z4++;
-            }
-            else {
-                this.b[1][1].setLabel("X");
-                this.a++;
-                this. z4++;
+
+        if (var1.getSource() == this.b[3] && this.z3 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[3].setLabel("O");
+                ++this.z3;
+                ++this.a;
+            } else {
+                this.b[3].setLabel("X");
+                ++this.z3;
+                ++this.a;
             }
         }
-        else if(e.getSource() == this.b[1][2] && z5!=1){
-            if(this.a%2==0){
-                this.b[1][2].setLabel("O");
-                this.a++;
-                this.z5++;
-            }
-            else {
-                this.b[1][2].setLabel("X");
-                this.a++;
-                this.z5++;
+
+        if (var1.getSource() == this.b[4] && this.z4 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[4].setLabel("O");
+                ++this.z4;
+                ++this.a;
+            } else {
+                this.b[4].setLabel("X");
+                ++this.z4;
+                ++this.a;
             }
         }
-        else if(e.getSource() == this.b[2][0]&& z6!=1){
-            if(this.a%2==0){
-                this.b[2][0].setLabel("O");
-                this.a++;
-                this.z6++;
-            }
-            else {
-                this.b[2][0].setLabel("X");
-                this.a++;
-                this.z6++;
-            }
-        }
-         else if(e.getSource() == this.b[2][1] && z7!=1){
-            if(a%2==0){
-                this.b[2][1].setLabel("O");
-                this.a++;
-                this.z7++;
-            }
-            else {
-                this.b[2][1].setLabel("X");
-                this.a++;
-                this.z7++;
+
+        if (var1.getSource() == this.b[5] && this.z5 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[5].setLabel("O");
+                ++this.z5;
+                ++this.a;
+            } else {
+                this.b[5].setLabel("X");
+                ++this.z5;
+                ++this.a;
             }
         }
-        else if(e.getSource() == this.b[2][2]&& z8!=1 ){
-            if(this.a%2==0){
-                this.b[2][2].setLabel("O");
-                this.a++;
-                this.z8++;
+
+        if (var1.getSource() == this.b[6] && this.z6 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[6].setLabel("O");
+                ++this.z6;
+                ++this.a;
+            } else {
+                this.b[6].setLabel("X");
+                ++this.z6;
+                ++this.a;
             }
-            else {
-                this.b[2][2].setLabel("X");
-                this.a++;
-                this. z8++;
+        }
+
+        if (var1.getSource() == this.b[7] && this.z7 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[7].setLabel("O");
+                ++this.z7;
+                ++this.a;
+            } else {
+                this.b[7].setLabel("X");
+                ++this.z7;
+                ++this.a;
             }
-        }}
+        }
 
+        if (var1.getSource() == this.b[8] && this.z8 == 0) {
+            if (this.a % 2 == 0) {
+                this.b[8].setLabel("O");
+                ++this.z8;
+                ++this.a;
+            } else {
+                this.b[8].setLabel("X");
+                ++this.z8;
+                ++this.a;
+            }
+        }
 
+        Font var4 = new Font("", 1, 20);
+        Label var3;
+        if (this.b[0].getLabel() == this.b[1].getLabel() && this.b[0].getLabel() == this.b[2].getLabel()) {
+            if (this.b[0].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
 
-        Font f = new Font("",Font.BOLD,20);
-        if((b[0][0].getLabel() == b[0][1].getLabel() && b[0][0].getLabel() == b[0][2].getLabel())){
-            if(b[0][0].getLabel() == "O"){
-                Label winner = new Label("Player 1 won");
-                winner.setSize(150,50);
-                winner.setLocation(320,20);
-                winner.setFont(f);
-                add(winner);
-                this.start = 0;
-            }}
+            if (this.b[0].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[3].getLabel() == this.b[4].getLabel() && this.b[4].getLabel() == this.b[5].getLabel()) {
+            if (this.b[3].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
 
-    }}
+            if (this.b[3].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[6].getLabel() == this.b[7].getLabel() && this.b[6].getLabel() == this.b[8].getLabel()) {
+            if (this.b[6].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
 
+            if (this.b[6].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[0].getLabel() == this.b[3].getLabel() && this.b[0].getLabel() == this.b[6].getLabel()) {
+            if (this.b[0].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
 
+            if (this.b[0].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[1].getLabel() == this.b[4].getLabel() && this.b[1].getLabel() == this.b[7].getLabel()) {
+            if (this.b[1].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
 
-public class Ticktactoee {
-    public static void main(String[] args) {
-        Fdemo f = new Fdemo();
+            if (this.b[1].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[2].getLabel() == this.b[5].getLabel() && this.b[5].getLabel() == this.b[8].getLabel()) {
+            if (this.b[2].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+
+            if (this.b[2].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[0].getLabel() == this.b[4].getLabel() && this.b[0].getLabel() == this.b[8].getLabel()) {
+            if (this.b[0].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+
+            if (this.b[0].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        } else if (this.b[2].getLabel() == this.b[4].getLabel() && this.b[2].getLabel() == this.b[6].getLabel()) {
+            if (this.b[2].getLabel() == "O") {
+                var3 = new Label("Player 1 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+
+            if (this.b[2].getLabel() == "X") {
+                var3 = new Label("Player 2 wins");
+                var3.setSize(150, 50);
+                var3.setLocation(320, this.l);
+                var3.setFont(var4);
+                this.add(var3);
+                this.l += 50;
+            }
+        }
 
     }
 }
